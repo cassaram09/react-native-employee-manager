26f659b52d804bc3459de66c412a15f850155bb7
cassaram09 <cassaram09@gmail.com>
2018-01-16 19:03:11 -0500

    finish authentication

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
+import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
+import { createStore, applyMiddleware } from 'redux';
+
+import reducers from './reducers';
+import LoginForm from './components/loginForm';
 
 class App extends Component<{}> {
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: "AIzaSyCZrIcQT46gho5UbCcB3vl9eqfgtsXsbjE",
-      authDomain: "manager-29eca.firebaseapp.com",
-      databaseURL: "https://manager-29eca.firebaseio.com",
-      projectId: "manager-29eca",
-      storageBucket: "manager-29eca.appspot.com",
-      messagingSenderId: "826260576741"
+      apiKey: 'AIzaSyCZrIcQT46gho5UbCcB3vl9eqfgtsXsbjE',
+      authDomain: 'manager-29eca.firebaseapp.com',
+      databaseURL: 'https://manager-29eca.firebaseio.com',
+      projectId: 'manager-29eca',
+      storageBucket: 'manager-29eca.appspot.com',
+      messagingSenderId: '826260576741'
     });
   }
 
   render() {
     return (
-      <Provider store={createStore(reducers)}>
-        <View>
-          <Text>Hello</Text>
-        </View>
+      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+       <LoginForm />
       </Provider>
     );
   }
